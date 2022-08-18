0d88c1f1a16909323fcee7bee71e906abf6f9752
Dobrovvek <dobrovvek@gmail.com>
2022-08-18 22:48:09 +0300

    task1+2

--- CSharp/HW1/Program.cs
+++ CSharp/HW1/Program.cs
@@ -1,7 +1,7 @@
-﻿// Задача 2/2 = (1)
+﻿// Задача 2 = (1)
 //Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
-
+/*
 int num1;
 Console.Write("напечатайте целое число, которое хотите сравнить: ");
 num1 = Convert.ToInt32(Console.ReadLine());
@@ -18,3 +18,34 @@ else {
     Console.Write(num2 + " больше чем " + num1);
 }
 
+
+*/
+//Задача 4 = (2)
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int num1;
+Console.Write("напечатайте первое целое число, которое хотите сравнить: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2;
+Console.Write("напечатайте второе целое число, которое хотите сравнить: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int num3;
+Console.Write("напечатайте третье целое число, которое хотите сравнить: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int maximum;
+
+
+if (num1 > num2) {
+    maximum = num1;
+}
+else {
+   maximum = num2;
+}
+if (num3 > maximum) {
+   Console.WriteLine("Наибольшее из трёх чисел - " + num3) ;
+}
+else {
+ Console.WriteLine("Наибольшее из трёх чисел - " + maximum) ;
+}
